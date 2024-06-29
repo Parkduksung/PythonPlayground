c29907a962f73db198f531bba2edc857aff2ec77
bagdeogseong <duksung1234@naver.com>
2024-06-29 17:13:47 +0900

    4.1 연산(1)

---
+++ myvenv/Chapter4/01.연산(1).py
@@ -0,0 +1,31 @@
+x= 5
+y = 2
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y) # 나누기
+print(x // y) # 몫
+print(x % y) # 나머지
+print(x ** y) # 제곱
+
+
+message = "우린 모두 파이썬을 사랍합니다.\n" * 5
+print(message)
+
+
+#복합할당 연산자
+
+level = 10 # 레벨 1증가
+level += 1
+
+health = 2000
+health -= 300
+
+attact = 300
+attact *= 1.5
+
+speed = 420
+speed /= 2
+
+print(level,health,attact,speed)
