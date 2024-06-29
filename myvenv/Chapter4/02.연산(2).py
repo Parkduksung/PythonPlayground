03503e65b912b518d4b5f885350cf0bb44afc22a
bagdeogseong <duksung1234@naver.com>
2024-06-29 17:21:24 +0900

    4.2 연산(2)

---
+++ myvenv/Chapter4/02.연산(2).py
@@ -0,0 +1,14 @@
+print(2 > 3)
+print(15 > 30)
+print(1.5 >= 0)
+print(3 <= 3)
+print("팙팙팙"  == "팙팙팙")
+print("11111111111" != "11111111111")
+print(4<6 and 10 >= 10)
+print(not 5==5)
+print("a" in "abc")
+print('a' in "abc")
+print('A' in "abc") # false
+print("d" not in "abc")
+
+
